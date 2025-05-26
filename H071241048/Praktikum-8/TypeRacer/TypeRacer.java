bc32ffa9bea2c9d243d9b623a024831df9a530ed
ShinZeleo <imamqhoir28@gmail.com>
2025-05-26 18:54:29 +0800

    Praktikum 8

---
+++ H071241048/Praktikum-8/TypeRacer/TypeRacer.java
@@ -0,0 +1,89 @@
+package TypeRacer;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class TypeRacer {
+    private String wordsToType;
+    private ArrayList<Typer> rareContestant = new ArrayList<>();
+    private ArrayList<Result> rareStanding = new ArrayList<>();
+
+    public String getWordsToType() {
+        return wordsToType;
+    }
+
+    public ArrayList<Typer> getRareContestant() {
+        return rareContestant;
+    }
+
+    // Bisa diganti sesuai keinginan masing-masing
+    private String[] wordsToTypeList = {
+            "Di Bikini Bottom ada Spongebob Squarepants, dia memang keren suka main drumband",
+            "Dia jadi koki masaknya krabby patty, menjalani hari hidup bersama Garry",
+            "Ayo sama-sama sebutkan nama-nama makhluk dalam sana di Bikini Bottom jaya",
+            "Namun ada juga namanya Patrick Star, walau dia cetar tapi hidupnya liar",
+            "Tinggal dalam batu tapi suka membantu, sayang hanya satu otaknya itu buntu"
+    };
+
+    public void setNewWordsToType() {
+        Random random = new Random();
+        int angkaRandom = random.nextInt(wordsToTypeList.length);
+        wordsToType = wordsToTypeList[angkaRandom];
+    }
+
+    // TODO (4)
+    // Buat method addResult yang mana digunakan untuk menambahkan typer yang
+    // telah selesai (mengetik semua kata), ke dalam list race standing.
+    public void addResult(Result result) {
+        rareStanding.add(result);
+    }
+
+
+    private void printRaceStanding() {
+        System.out.println("\nKlasemen Akhir Type Racer");
+        System.out.println("=========================\n");
+
+        // TODO (5)
+        // Tampilkan klasemen akhir dari kompetisi, dengan format
+        // {posisi}. {nama} = {waktu penyelesaian dalam detik} detik
+        rareStanding.sort((a, b) -> a.getFinishTime() - b.getFinishTime());
+
+        for (int i = 0; i < rareStanding.size(); i++) {
+            Result result = rareStanding.get(i);
+            System.out.printf("%d. %s = %d detik\n", i+1, result.getName(), result.getFinishTime());
+        }
+    }
+
+    public void startRace() {
+        // TODO (6)
+        // Jalankan kompetisi untuk tiap kontestan
+        for (Typer bot : rareContestant) {
+            bot.start();
+        }
+    }
+
+    // TODO (7)
+    // selama semua peserta belum selesai maka tampilkan typing progress-nya setiap
+    // 2 detik, dengan format:
+    // Typing Progress ...
+    // ===================
+    // {nama kontestan} => {text yang telah dia ketik}
+    // {nama kontestan} => {text yang telah dia ketik}
+    // {nama kontestan} => {text yang telah dia ketik}
+    public void displayRaceStandingPeriodically() throws InterruptedException {
+        while (rareStanding.size() < rareContestant.size()) {
+            Thread.sleep(2000);
+            System.out.println("\nTyping Progress...");
+            System.out.println("===================");
+
+            for (Typer typer : rareContestant) {
+                System.out.printf("%s => %s\n", typer.getBotName(), typer.getWordsTyped());
+            }
+        }
+
+        // TODO (8)
+        // Setelah semua typer selesai, tampilkan race standing setelah semua typer
+        // selesai
+        printRaceStanding();
+    }
+}
